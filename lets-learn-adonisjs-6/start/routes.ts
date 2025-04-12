a0090f7c5dc5ec5302a231af36fdcf45033884a4
Bernardo Rosmaninho <bernardo.rosmaninho@gmail.com>
2025-04-12 19:35:08 -0300

    Aprendendo sobre routes, alias das views no AdonisJS 6

--- lets-learn-adonisjs-6/start/routes.ts
+++ lets-learn-adonisjs-6/start/routes.ts
@@ -9,4 +9,16 @@
 
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home')
+router.on('/').render('pages/home').as('home')
+
+router
+  .get('/movies', (ctx) => {
+    return ctx.view.render('pages/movies', { movie: 'Tron: Legacy' })
+  })
+  .as('movies.index')
+router.get('/movies/my-movie', () => {}).as('movies.show')
+router.get('/movies/create', () => {}).as('movies.create')
+router.get('/movies/my-movie/edit', () => {}).as('movies.edit')
+router.post('/movies', () => {}).as('movies.store')
+router.put('/movies/my-movie', () => {}).as('movies.update')
+router.delete('/movies/my-movie', () => {}).as('movies.destroy')
